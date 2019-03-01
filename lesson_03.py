600af84bf919adf9473f15fe1cf402fd9aaa4d89
Andrey <afanto88@gmail.com>
2019-03-01 23:15:43 +0300

    Add "/" operator to lesson_03

--- lesson_03.py
+++ lesson_03.py
@@ -31,7 +31,7 @@ class CalculatorApp(App):
 
         self.update_label()
 
-    def calc_result(self):
+    def calc_result(self, instance):
         self.lbl.text = str(eval(self.lbl.text))
         self.formula = "0"
 
@@ -40,7 +40,7 @@ class CalculatorApp(App):
         self.formula = "0"
 
         bl = BoxLayout(orientation="vertical", padding=5)
-        gl = GridLayout(cols=4, spacing=3, size_hint=(1, .6))
+        gl = GridLayout(cols=4, rows=5, spacing=3, size_hint=(1, .6))
 
         self.lbl = Label(text="0",
                          font_size=40,
@@ -50,6 +50,11 @@ class CalculatorApp(App):
                          text_size=(400 - 50, 500 * .4 - 50))
 
         bl.add_widget(self.lbl)
+
+        gl.add_widget(Widget())
+        gl.add_widget(Widget())
+        gl.add_widget(Widget())
+        gl.add_widget(Button(text="/", on_press=self.add_operator))
 
         gl.add_widget(Button(text="7", on_press=self.add_number))
         gl.add_widget(Button(text="8", on_press=self.add_number))
